27e2f42b16a458ced53f06f23bff821cec30990e
banteg <4562643+banteg@users.noreply.github.com>
2022-08-12 02:43:27 +0400

    feat: add deployment

---
+++ readme.md
@@ -0,0 +1,5 @@
+# staking deposit 
+
+mass deposit goerli validators
+
+deployed at 0x5B0804665A57D81Ea5992773D5D5e1b10aAe956B
--- scripts/deposit.py
+++ scripts/deposit.py
@@ -5,9 +5,10 @@ from toolz import count
 
 def main():
     assert chain.provider.network.name.startswith("goerli")
-    user = accounts.load('goerli')
+    user = accounts.load("goerli")
     deposit_data = json.load(open("deposit_data.json"))
     deposit_contract = Contract("0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b")
+    mass_deposit = project.MassDeposit.at("0x5B0804665A57D81Ea5992773D5D5e1b10aAe956B")
     datas = [
         [
             d["pubkey"],
@@ -18,6 +19,5 @@ def main():
         for d in deposit_data
     ]
     value = 32 * 10**18 * len(datas)
-    mass_deposit = project.MassDeposit.deploy(sender=user)
     receipt = mass_deposit.deposit_many(datas, value=value, sender=user)
     assert count(deposit_contract.DepositEvent.from_receipt(receipt)) == len(datas)
