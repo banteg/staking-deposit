24ebce48c8f8249655495d1868d0140ff7002e41
banteg <4562643+banteg@users.noreply.github.com>
2022-08-12 02:47:34 +0400

    feat: send in batches of 100

--- scripts/deposit.py
+++ scripts/deposit.py
@@ -1,6 +1,6 @@
 import json
 from ape import chain, Contract, accounts, project
-from toolz import count
+from toolz import count, partition_all
 
 
 def main():
@@ -9,15 +9,17 @@ def main():
     deposit_data = json.load(open("deposit_data.json"))
     deposit_contract = Contract("0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b")
     mass_deposit = project.MassDeposit.at("0x5B0804665A57D81Ea5992773D5D5e1b10aAe956B")
-    datas = [
-        [
-            d["pubkey"],
-            d["withdrawal_credentials"],
-            d["signature"],
-            d["deposit_data_root"],
+
+    for batch in partition_all(100, deposit_data):
+        datas = [
+            [
+                d["pubkey"],
+                d["withdrawal_credentials"],
+                d["signature"],
+                d["deposit_data_root"],
+            ]
+            for d in batch
         ]
-        for d in deposit_data
-    ]
-    value = 32 * 10**18 * len(datas)
-    receipt = mass_deposit.deposit_many(datas, value=value, sender=user)
-    assert count(deposit_contract.DepositEvent.from_receipt(receipt)) == len(datas)
+        value = 32 * 10**18 * len(datas)
+        receipt = mass_deposit.deposit_many(datas, value=value, sender=user)
+        assert count(deposit_contract.DepositEvent.from_receipt(receipt)) == len(datas)
